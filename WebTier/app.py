af4c5e08718455d32f4c0dc8537c6055d49a19a3
Kevin Middelmann <kevin_middelmann@gmx.de>
2020-10-21 14:03:32 +0100

    credentials

--- WebTier/app.py
+++ WebTier/app.py
@@ -1,21 +1,43 @@
+import mysql.connector
+from flask import jsonify
 
 import mysql.connector
 import time
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_cors import CORS
-from datetime import datetime, timedelta
 import json
 
 
+SECRET_KEY = "greentickteamforthewin"
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/login')
-def login(json):
-    usernama =
-    password =
-    cnx = mysql.connector.connect(user='admin', password='ppp',host="127.0.0.1",port=3306,database='db_grad_cs_1917')
-    return Response(login.html, status=200, mimetype='application/json')
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+
+    content = request.json
+    testdata = '{"username":"selvyn", "password":"gradprog2016"}'
+    credentials = json.loads(content)
+    cnx = mysql.connector.connect(user='root', password='ppp', host="localhost", port=3306, database='db_grad_cs_1917')
+
+    cursor = cnx.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE user_id = %s AND user_pwd = %s',
+                   (credentials["username"], credentials["password"]))
+
+    account = cursor.fetchone()
+
+    # If account exists in accounts table in out database
+    if account:
+        # Create session data, we can access this data in other routes
+        print('Logged in successfully!')
+        response_dict = {"token": SECRET_KEY}
+        return jsonify(response_dict)
+    else:
+        # Account doesnt exist or username/password incorrect
+        print('Incorrect username/password!')
+        return False
+
 
 @app.route('/statistics')
 def statistics():
--- WebTier/test.py
+++ WebTier/test.py
@@ -1,11 +1,27 @@
 import mysql.connector
+import json
+from flask import jsonify
+
+
+SECRET_KEY = "greentickteamforthewin"
+
+testdata = '{"username":"selvyn", "password":"gradprog2016"}'
+credentials = json.loads(testdata)
 cnx = mysql.connector.connect(user='root', password='ppp', host="localhost", port=3306, database='db_grad_cs_1917')
 
-mycursor = cnx.cursor()
+cursor = cnx.cursor()
 
-mycursor.execute("SELECT * FROM users")
+cursor.execute('SELECT * FROM users WHERE user_id = %s AND user_pwd = %s', (credentials["username"], credentials["password"]))
 
-myresult = mycursor.fetchall()
+account = cursor.fetchone()
 
-for x in myresult:
-    print(x)
+# If account exists in accounts table in out database
+if account:
+    # Create session data, we can access this data in other routes
+    print( 'Logged in successfully!')
+    response_dict={"TOKEN":SECRET_KEY}
+    return jsonify(response_dict)
+else:
+    # Account doesnt exist or username/password incorrect
+    print('Incorrect username/password!')
+
