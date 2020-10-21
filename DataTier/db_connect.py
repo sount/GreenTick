dcfbe4ef4938b84041b686b2e7f618f8fa18cea1
KaChunWan <kachunwn1@gmail.com>
2020-10-21 13:59:55 +0100

    Use sseclient to get json data

--- DataTier/db_connect.py
+++ DataTier/db_connect.py
@@ -1,22 +1,34 @@
 import mysql.connector
+from sseclient import SSEClient
 import requests
 from requests.exceptions import HTTPError
+import json
+
+#
+# def get_deal_data():
+#
+#     try:
+#         response = requests.get('http://127.0.0.1:5000/testservice')
+#         response.raise_for_status()
+#         # access JSON content
+#         jsonResponse = response.json()
+#         print("Entire JSON response")
+#         print(jsonResponse)
+#
+#     except HTTPError as http_err:
+#         print(f'HTTP error occurred: {http_err}')
+#     except Exception as err:
+#         print(f'Other error occurred: {err}')
+#
+#     return jsonResponse
 
 
-def get_deal_data():
+def _streamSSE(url):
+    '''internal'''
+    messages = SSEClient(url)
+    for msg in messages:
+        print(msg)
 
-    try:
-        response = requests.get('http://127.0.0.1:5000/streamTest/sse')
-        response.raise_for_status()
-        # access JSON content
-        jsonResponse = response.json()
-        print("Entire JSON response")
-        print(jsonResponse)
-
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred: {err}')
 
 
 def connect_to_db():
@@ -27,4 +39,5 @@ def connect_to_db():
 
 
 if __name__ == '__main__':
-    get_deal_data()
+    _streamSSE('http://127.0.0.1:5000/streamTest/sse')
+    # get_deal_data()
