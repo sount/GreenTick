7aeadb316f688f54f21c3eacc68461040af18a24
KaChunWan <kachunwn1@gmail.com>
2020-10-22 17:42:04 +0100

    added functions to calculate average buy and sell prices of instruments and profit/loss of each counterparty

--- data-generator/statistic_calculation.py
+++ data-generator/statistic_calculation.py
@@ -1,18 +1,63 @@
 
 import mysql.connector
 import pandas as pd
+import numpy as np
 
 cnx = mysql.connector.connect(user='root', password='ppp', host="localhost", port=3306,
                                       database='db_grad_cs_1917')
 
+instrument_name_to_id = {"Astronomica" : 1001,
+                         "Borealis" : 1002,
+                         "Celestial" : 1003,
+                         "Deuteronic" : 1004,
+                         "Eclipse" : 1005,
+                         "Floral" : 1006,
+                         "Galactia" : 1007,
+                         "Heliosphere" : 1008,
+                         "Interstella" : 1009,
+                         "Jupiter" : 1010,
+                         "Koronis" : 1011,
+                         "Lunatic" : 1012}
+instrument_id_to_name = {b : a for a, b in instrument_name_to_id.items()}
+
+ctpy_name_to_id = {"Lewis" : 701,
+                   "Selvyn" : 702,
+                   "Richard" : 703,
+                   "Lina" : 704,
+                   "John" : 705,
+                   "Nidia" : 706}
+ctpy_id_to_name = {b : a for a, b in ctpy_name_to_id.items()}
+
 query = 'SELECT * FROM deal'
 # save everything for the calculations in a dataframe
-data_dataframe = pd.read_sql(query, cnx)
-instrument_id = data_dataframe["deal_instrument_id"].unique().sort()
-print(instrument_id)
+data = pd.read_sql(query, cnx)
 
-grouped_data= data_dataframe.groupby(['deal_instrument_id', 'deal_type'])[["deal_quantity","deal_amount"]]
-print(grouped_data.describe())
-#.size().unstack(level=1)
-#make dataframe to json
-response_dict = data_dataframe.to_json()
+
+def find_dealer_PL(data_dataframe):
+
+    ctpy_PL = {}
+    for index, row in data_dataframe.iterrows():
+        if row['deal_type'] == "B":
+            modifier = -1
+        else:
+            modifier = 1
+        if ctpy_id_to_name[row['deal_counterparty_id']] not in list(ctpy_PL.keys()):
+            ctpy_PL[ctpy_id_to_name[row['deal_counterparty_id']]] = (row['deal_amount'] * row['deal_quantity'] * modifier)
+        else:
+            ctpy_PL[ctpy_id_to_name[row['deal_counterparty_id']]] += (row['deal_amount'] * row['deal_quantity'] * modifier)
+
+    ctpy_PL_df = pd.DataFrame(list(ctpy_PL.items()), columns=['deal_counterparty', 'PL'])
+
+    return ctpy_PL_df
+
+
+def find_average_buy_sell_price(data_dataframe):
+    pd.set_option('max_columns', None)
+    grouped_data = data_dataframe.groupby(['deal_instrument_id', 'deal_type'])[["deal_quantity", "deal_amount"]]
+
+    return grouped_data.mean()
+
+
+if __name__ == '__main__':
+    # find_dealer_PL(data)
+    find_average_buy_sell_price(data)
